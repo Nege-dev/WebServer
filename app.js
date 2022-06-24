85b27a9c9f4e729a11d2125df8edb5a6bdd79ff2
Nimco <t-nimoege@microsoft.com>
2022-06-24 11:14:25 +0100

    added date time functionality

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  
+  let today = new Date();
+  let date = today.toLocaleDateString();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+ 
+  const response = {
+    Name: 'Time and Date server', 
+    Date: date,
+    Time: time
+  };
+  
+  res.end(JSON.stringify(response));
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(3000);
